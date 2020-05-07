e3006dac9219b13028d9b3bae295b4fb314ccdbd
Gong_yihang <474356284@qq.com>
2020-05-07 16:25:37 +0800

    5.7

---
+++ data96240.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+data962400 = [np.random.permutation(96) for _ in range(240)]
+np.savetxt("data962400.txt",data962400,fmt="%d")
+
+data962401 = [np.random.permutation(96) for _ in range(240)]
+np.savetxt("data962401.txt",data962401,fmt="%d")
+
+data962402 = [np.random.permutation(96) for _ in range(240)]
+np.savetxt("data962402.txt",data962402,fmt="%d")
+
+data962403 = [np.random.permutation(96) for _ in range(240)]
+np.savetxt("data962403.txt",data962403,fmt="%d")
+
+data96960 = [np.random.permutation(96) for _ in range(960)]
+np.savetxt("data962403.txt",data96960,fmt="%d")
